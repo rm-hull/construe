b587554bbd115c21a87776a30e5d7807571b7eed
Richard Hull <rm_hull@yahoo.co.uk>
2023-10-29 17:10:00 +0000

    Introduce chakra-ui & error-boundary

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,21 @@
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Code, Container, Heading } from "@chakra-ui/react";
+import type { FallbackProps } from "react-error-boundary";
+
+export default function ErrorFallback({ error }: FallbackProps): JSX.Element {
+  return (
+    <Container maxWidth="container.lg">
+      <Alert status="error">
+        <AlertIcon />
+        <AlertTitle>Something went wrong:</AlertTitle>
+        <AlertDescription>{(error as Error).message}</AlertDescription>
+      </Alert>
+
+      <Container m={5}>
+        <Heading size="sm">Stack trace</Heading>
+        <Code background="none">
+          <pre>{(error as Error).stack}</pre>
+        </Code>
+      </Container>
+    </Container>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import { ChakraProvider, ColorModeScript, createLocalStorageManager, theme } from "@chakra-ui/react";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import ErrorFallback from "./components/ErrorBoundary";
 
 const container = document.getElementById("root");
 if (container == null) {
@@ -12,15 +15,21 @@ if (container == null) {
 }
 
 const root = createRoot(container);
+const manager = createLocalStorageManager("construe.color-mode");
 const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
+    <ColorModeScript storageKey="maps.color-mode" />
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <BrowserRouter basename="/construe">
-        <App />
-      </BrowserRouter>
+      <ChakraProvider theme={theme} colorModeManager={manager}>
+        <BrowserRouter basename="/construe">
+          <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <App />
+          </ErrorBoundary>
+        </BrowserRouter>
+      </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
