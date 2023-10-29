d08391fe4f94710d3f781e22023d2d287d729e53
Richard Hull <rm_hull@yahoo.co.uk>
2023-10-29 11:58:33 +0000

    Add default route

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<div>About</div>} />
+        <Route path="*" element={<div>Not found</div>} />
       </Routes>
     </BrowserRouter>
   );
